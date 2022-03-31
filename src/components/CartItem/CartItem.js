4859d7283c8070c53d643c7ff2bcd8a8e2f9d5d7
Ruman-Islam <rumanislam48@gmail.com>
2022-03-31 22:59:05 +0600

    cart item done

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -3,20 +3,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import './CartItem.css';
 
-const CartItem = ({ product: { name, price, shipping, quantity, img } }) => {
+const CartItem = ({ handleRemoveFromCart, product, product: { name, price, shipping, quantity, img } }) => {
+
     return (
         <div className='cart-item'>
             <div className='item-img-container'>
                 <img src={img} alt="" />
             </div>
             <div className='item-detail-container'>
-                <h5 title={name} className='detail-title'>{name.length > 20 ? name.slice(2, 20) + '...' : name}</h5>
+                <h6 title={name} className='detail-title'>{name.length > 10 ? name.slice(2, 20) + '...' : name}</h6>
                 <p className='detail-title'>Price: $ {price}</p>
                 <p className='detail-title'>Shipping Cost: $ {shipping}</p>
                 <p className='detail-title'>Quantity: {quantity}</p>
             </div>
             <div className='remove-item-btn-container'>
-                <FontAwesomeIcon className='icon' icon={faTrashCan} />
+                <button onClick={() => handleRemoveFromCart(product)}>
+                    <FontAwesomeIcon className='icon' icon={faTrashCan} />
+                </button>
             </div>
         </div>
     );
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/utilitiesFunctions';
 import Cart from '../Cart/Cart';
 import CartItem from '../CartItem/CartItem';
 import './Orders.css';
 
 const Orders = ({ setItemsCount }) => {
     const [products] = useProducts();
-    const [cart] = useCart(products);
+    const [cart, setCart] = useCart(products);
+
+    const handleRemoveFromCart = (selectedItem) => {
+        const restItems = cart.filter(cartItem => cartItem.id !== selectedItem.id);
+        setCart(restItems);
+        removeFromDb(selectedItem.id);
+    }
+
     return (
         <div className='main-container container'>
             <div className="container cart-item-container">
                 {
-                    cart.map(product => <CartItem key={product.key} product={product} />)
+                    cart.map(product => <CartItem key={product.key}
+                        product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                    />)
                 }
             </div>
             <div className="cart-detail-container">
-                <Cart setItemsCount={setItemsCount} cart={cart} />
+                <Cart
+                    setItemsCount={setItemsCount}
+                    cart={cart}
+                    setCart={setCart}
+                />
             </div>
         </div>
     );
