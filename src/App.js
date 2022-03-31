b83978229025328b4dcdf756ab032f695ff8e987
Ruman-Islam <rumanislam48@gmail.com>
2022-03-31 18:36:21 +0600

    react router installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [itemsCount, setItemsCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header itemsCount={itemsCount} />
       <Shop setItemsCount={setItemsCount} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Orders.css';
+
+const Orders = () => {
+    return (
+        <div>
+            <h1>This is Order Component</h1>
+        </div>
+    );
+};
+
+export default Orders;
